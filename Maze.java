6dc31f81cdd6c828e51388ba4a7caf86c4684ef6
Karl Obermiller <obermiller.k@husky.neu.edu>
2016-04-10 17:19:18 -0400

    Added draw functionality to Mazes.

--- Maze.java
+++ Maze.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+import javalib.impworld.*;
 
 public class Maze {
-
+    ArrayList<Cell> cells;
+    
+    Maze() {
+        this.cells = new ArrayList<Cell>();
+        for (int r = 0; r < 20; r ++) {
+            for (int c = 0; c < 40; c++) {
+                this.cells.add(new Cell(r, c));
+            }
+        }
+    }
+    
+    WorldScene draw(WorldScene base) {
+        for (Cell c: this.cells) {
+            base = c.draw(base);
+        }
+        
+        return base;
+    }
 }
