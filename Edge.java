c76558c6d28a2c2692dc2ff32cf1b73dd0134dc8
Karl Obermiller <obermiller.k@husky.neu.edu>
2016-04-10 17:44:41 -0400

    Created Edge class.

---
+++ Edge.java
@@ -0,0 +1,11 @@
+class Edge {
+    int weight;
+    Cell cell1;
+    Cell cell2;
+    
+    Edge(Cell cell1, Cell cell2, int weight) {
+        this.cell1 = cell1;
+        this.cell2 = cell2;
+        this.weight = weight;
+    }
+}
