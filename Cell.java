3c17466e8a946ee0a7b4f95f6b105f248f1bdf65
Karl Obermiller <obermiller.k@husky.neu.edu>
2016-04-10 17:17:57 -0400

    Added basic draw Functionality to cells.

--- Cell.java
+++ Cell.java
@@ -3,28 +3,52 @@ import javalib.worldimages.*;
 import java.awt.Color;
 
 class Cell extends GMember {
-    static final int SIZE = 10;
+    static final int SIZE = 20;
     
     int r;
     int c;
-    boolean onRight;
-    boolean onBottom;
+    boolean rightWall;
+    boolean bottomWall;
     boolean wasVisited;
     boolean isOnPath;
     
-    Cell() {
+    Cell(int r, int c) {
         super();
+        this.r = r;
+        this.c = c;
+        this.rightWall = true;
+        this.bottomWall = true;
+        this.wasVisited = true;
+        this.isOnPath = (r % 2) == 1;
     }
     
     WorldScene draw(WorldScene base) {
-        WorldImage image = new RectangleImage(SIZE, SIZE, "solid", Color.BLUE);
-        image = new OverlayOffsetImage(
-                new LineImage(new Posn(SIZE, 0), Color.BLACK),
-                SIZE / 2,
-                0,
-                image);
+        Color color = new Color(0xE0E0E0);
+        if (this.wasVisited) {
+            color = new Color(0x80D0FF);
+        }
+        if (this.isOnPath) {
+            color = new Color(0x4080FF);
+        }
+        WorldImage image = new RectangleImage(SIZE, SIZE, "solid", color);
+        if (this.rightWall) {
+            image = new OverlayOffsetImage(
+                    new LineImage(new Posn(0, SIZE), Color.BLACK),
+                    -SIZE / 2 + 1,
+                    0,
+                    image);
+        }
+        if (this.bottomWall) {
+            image = new OverlayOffsetImage(
+                    new LineImage(new Posn(SIZE, 0), Color.BLACK),
+                    0,
+                    -SIZE / 2 + 1,
+                    image);
+        }
         
-        base.placeImageXY(image, r * SIZE, c*SIZE);
+        base.placeImageXY(image,
+                c * SIZE + SIZE / 2,
+                r * SIZE + SIZE / 2);
         
         return base;
     }
