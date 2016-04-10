0c9b5a5e4e6949e6c22c5c3e8b1dea45503b50e8
Karl Obermiller <obermiller.k@husky.neu.edu>
2016-04-10 17:18:33 -0400

    Created test method to run game.

--- ExamplesMaze.java
+++ ExamplesMaze.java
@@ -1,4 +1,21 @@
+import tester.*;
 
-public class ExamplesMaze {
-
+class ExamplesMaze {
+    // Runs the game.
+    void testMaze(Tester t) {
+        MazeWorld world = new MazeWorld();
+        world.bigBang(MazeWorld.WIDTH, MazeWorld.HEIGHT);
+    }
 }
+
+/**
+ * To test:
+ *  GMember.resetLeader();
+ *  GMember.setLeader();
+ *  GMember.find();
+ *  GMember.union();
+ *  
+ *  GLeader.groupSize();
+ *  GLeader.union();
+ *  GLeader.addMember();
+ */
