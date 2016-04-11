0848492a16c7ff142eea723a56da724e808e044f
Karl Obermiller <obermiller.k@husky.neu.edu>
2016-04-11 15:54:14 -0400

    Added current Cell field, fixed draw method, and added move method.

--- Player.java
+++ Player.java
@@ -8,21 +8,57 @@ class Player
 {
     int row; // row position
     int col; // col position
+    Cell current;
     
     // ctor 
-    Player(int row, int col)
+    Player(int row, int col, Cell current)
     {
         this.row = row;
         this.col = col;
+        this.current = current;
     }
     
+    // Draws this player on the give base.
     WorldScene draw(WorldScene base)
     {
-        WorldImage image = new RectangleImage(Cell.SIZE, Cell.SIZE, 
+        WorldImage image = new RectangleImage(Cell.SIZE - 1, Cell.SIZE - 1, 
                 "solid", Color.MAGENTA);
 
         base.placeImageXY(image, 
-                this.row * Cell.SIZE, this.col * Cell.SIZE);
+                this.col * Cell.SIZE + Cell.SIZE / 2,
+                this.row * Cell.SIZE + Cell.SIZE / 2);
         return base;
+    }
+    
+    // Moves the player in the given direction, if possible.
+    // EFFECT: Updates r and c to new position and changes current cell, if possible.
+    void move(String dir) {
+        if (dir.equals("up")) {
+            if (this.current.top != null) {
+                this.row -= 1;
+                this.current = this.current.top.getOtherCell(this.current);
+            }
+        }
+        else if (dir.equals("right")) {
+            if (this.current.right != null) {
+                this.col += 1;
+                this.current = this.current.right.getOtherCell(this.current);
+            }
+        }
+        else if (dir.equals("down")) {
+            if (this.current.bottom != null) {
+                this.row += 1;
+                this.current = this.current.bottom.getOtherCell(this.current);
+            }
+        }
+        else if (dir.equals("left")) {
+            if (this.current.left != null) {
+                this.col -= 1;
+                this.current = this.current.left.getOtherCell(this.current);
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Must be a direction.");
+        }
     }
 }
