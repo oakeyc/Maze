e3c2c8ba6c4f4ee7abfdc173c4d530d70734650c
Karl Obermiller <obermiller.k@husky.neu.edu>
2016-04-15 17:52:11 -0400

    Moved parents list into only depth solver, as that was the only one using it.

--- Solver.java
+++ Solver.java
@@ -3,15 +3,12 @@ import java.util.ArrayList;
 // A maze solver.
 abstract class ASolver {
     ArrayList<Cell> toVisit;
-    ArrayList<Cell> parents;
     ArrayList<Cell> path;
     boolean solved;
     
     ASolver(Cell current) {
         this.toVisit = new ArrayList<Cell>();
         this.toVisit.add(current);
-        this.parents = new ArrayList<Cell>();
-        this.parents.add(current);
         this.path = new ArrayList<Cell>();
         this.path.add(current);
         this.solved = false;
@@ -20,11 +17,10 @@ abstract class ASolver {
     // Runs the next step of the solving algorithm.
     boolean nextStep() {
         Cell current = this.nextCell();
-        Cell parent = this.nextParent();
         current.wasVisited = true;
         
         //Remove all from path until it reaches the parent cell.
-        this.handlePath(current, parent);
+        this.handlePath(current);
         
         // Check for end.
         if (current.isEndCell()) {
@@ -50,16 +46,11 @@ abstract class ASolver {
         return this.nextInList(this.toVisit);
     }
     
-    // Returns the next cell in the parents list.
-    Cell nextParent() {
-        return this.nextInList(this.parents);
-    }
-    
     // Returns the next cell in the given list.
-    abstract Cell nextInList(ArrayList<Cell> cellList);
+    abstract <T> T nextInList(ArrayList<T> cellList);
     
     // Handles getting the path to the current cell.
-    abstract void handlePath(Cell current, Cell parent);
+    abstract void handlePath(Cell current);
     
     // Adds the given cell to the que, and may perform other actions associated
     //   with adding that cell.
@@ -68,22 +59,26 @@ abstract class ASolver {
 
 // A maze solver that uses depth first search.
 class DepthSolver extends ASolver {
+    ArrayList<Cell> parents;
     
     DepthSolver(Cell current) {
         super(current);
+        this.parents = new ArrayList<Cell>();
+        this.parents.add(current);
     }
     
     @Override
     // Returns the next cell at the end of the given list.
     // EFFECT: Removes the last cell from the given list.
-    public Cell nextInList(ArrayList<Cell> cellList) {
-        return cellList.remove(cellList.size() - 1);
+    public <T> T nextInList(ArrayList<T> list) {
+        return list.remove(list.size() - 1);
     }
     
     @Override
     // EFFECT: Resets the path back to the parent of the current cell,
     //         then adds the current cell.
-    public void handlePath(Cell current, Cell parent) {
+    public void handlePath(Cell current) {
+        Cell parent = this.nextParent();
         while (!this.path.get(this.path.size() - 1).equals(parent)) {
             this.path.remove(this.path.size() - 1);
         }
@@ -97,6 +92,12 @@ class DepthSolver extends ASolver {
     public void addCellToQue(Cell toAdd, Cell current) {
         this.toVisit.add(toAdd);
         this.parents.add(current);
+    }
+    
+
+    // Returns the next cell in the parents list.
+    Cell nextParent() {
+        return this.nextInList(this.parents);
     }
 }
 
@@ -114,15 +115,15 @@ class BreadthSolver extends ASolver {
     @Override
     // Returns the next cell at the beginning of the given list.
     // EFFECT: Removes the first cell from the given list.
-    public Cell nextInList(ArrayList<Cell> cellList) {
-        return cellList.remove(0);
+    public <T> T nextInList(ArrayList<T> list) {
+        return list.remove(0);
     }
     
     @Override
     // EFFECT: Resets the path to a copy of the path of the current cell's parent
     //         (which is stored in paths), then adds the current cell to that path.
-    public void handlePath(Cell current, Cell parent) {
-        this.path = (ArrayList<Cell>)this.paths.remove(0).clone();
+    public void handlePath(Cell current) {
+        this.path = (ArrayList<Cell>)(this.nextInList(this.paths).clone());
         this.path.add(current);
     }
     
@@ -131,7 +132,6 @@ class BreadthSolver extends ASolver {
     //         as its parent to the parents list.
     public void addCellToQue(Cell toAdd, Cell current) {
         this.toVisit.add(toAdd);
-        this.parents.add(current);
         this.paths.add(this.path);
     }
 }
